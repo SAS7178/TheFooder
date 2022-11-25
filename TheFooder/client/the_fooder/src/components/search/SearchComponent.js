383e65da417384618c1a04debfbad69bb4ed7a29
Stephen Smith <StephenASmith7178@gmail.com>
2022-11-25 10:29:51 -0500

    setting default search state when empty

--- TheFooder/client/the_fooder/src/components/search/SearchComponent.js
+++ TheFooder/client/the_fooder/src/components/search/SearchComponent.js
@@ -7,33 +7,72 @@ export const SearchComponent = ({ recipeObject, getAllRecipes }) => {
     const [quickModal, setQuickModal] = useState(false);
     const quickToggle = () => setQuickModal(!quickModal);
     const bool = false;
-    return (
-        <div className="searchBarDrop">
-            <div className='johns'>
-                <img alt="" src="https://www.pngall.com/wp-content/uploads/11/Horizontal-Line-PNG-Image.png" width="100%" height="35em"></img>
-                <strong>{recipeObject.name}</strong>
-                <Button id="dropDown" color="danger" onClick={quickToggle}>
-                    View Recipe
-                </Button>
-            </div>
-            <img alt="" src="https://www.pngall.com/wp-content/uploads/11/Horizontal-Line-PNG-Image.png" width="100%" height="35em"></img>
 
-            <Modal isOpen={quickModal} toggle={quickToggle} {...recipeObject}>
-                <ModalHeader className='quickView' toggle={quickToggle}>Quick View</ModalHeader>
-                <ModalBody id='modal-content'>
-                    <>
-                        <section>{getAllRecipes}</section>
-                        <section className='quickSearchView'>
-                            <Recipe recipe={recipeObject} key={recipeObject.id} isSavedRecipe={bool} />
-                        </section>
-                    </>
-                </ModalBody>
-                <ModalFooter>
-                    <Button color="secondary" onClick={quickToggle}>
-                        Close
+    // const handleNoRecipe = (recipeObj) => {
+   
+    //         <div className="searchBarDrop">
+    //             <div className='johns'>
+    //                 <img alt="" src="https://www.pngall.com/wp-content/uploads/11/Horizontal-Line-PNG-Image.png" width="100%" height="35em"></img>
+    //                 <strong>{recipeObject.name}</strong>
+    //                 <Button id="dropDown" color="danger" onClick={quickToggle}>
+    //                     View Recipe
+    //                 </Button>
+    //             </div>
+    //             <img alt="" src="https://www.pngall.com/wp-content/uploads/11/Horizontal-Line-PNG-Image.png" width="100%" height="35em"></img>
+
+    //             <Modal isOpen={quickModal} toggle={quickToggle} {...recipeObject}>
+    //                 <ModalHeader className='quickView' toggle={quickToggle}>Quick View</ModalHeader>
+    //                 <ModalBody id='modal-content'>
+    //                     <>
+    //                         <section>{getAllRecipes}</section>
+    //                         <section className='quickSearchView'>
+    //                             <Recipe recipe={recipeObject} key={recipeObject.id} isSavedRecipe={bool} />
+    //                         </section>
+    //                     </>
+    //                 </ModalBody>
+    //                 <ModalFooter>
+    //                     <Button color="secondary" onClick={quickToggle}>
+    //                         Close
+    //                     </Button>
+    //                 </ModalFooter>
+    //             </Modal>
+    //         </div>
+    //     } 
+    
+
+    return (
+        <>
+        {/* {
+        handleNoRecipe(recipeObject)
+        } */}
+                    <div className="searchBarDrop">
+                <div className='johns'>
+                    <img alt="" src="https://www.pngall.com/wp-content/uploads/11/Horizontal-Line-PNG-Image.png" width="100%" height="35em"></img>
+                    <strong>{recipeObject.name}</strong>
+                    <Button id="dropDown" color="danger" onClick={quickToggle}>
+                        View Recipe
                     </Button>
-                </ModalFooter>
-            </Modal>
-        </div>
+                </div>
+                <img alt="" src="https://www.pngall.com/wp-content/uploads/11/Horizontal-Line-PNG-Image.png" width="100%" height="35em"></img>
+
+                <Modal isOpen={quickModal} toggle={quickToggle} {...recipeObject}>
+                    <ModalHeader className='quickView' toggle={quickToggle}>Quick View</ModalHeader>
+                    <ModalBody id='modal-content'>
+                        <>
+                            <section>{getAllRecipes}</section>
+                            <section className='quickSearchView'>
+                                <Recipe recipe={recipeObject} key={recipeObject.id} isSavedRecipe={bool} />
+                            </section>
+                        </>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="secondary" onClick={quickToggle}>
+                            Close
+                        </Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+
+        </>
     );
 }
